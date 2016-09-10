03a17aa24a84147733ce6131eef16e54f91eaa19
Main <Main@Main>
2016-09-10 15:53:02 -0500

    more objects

--- FBP/src/fbp/FBPmain.java
+++ FBP/src/fbp/FBPmain.java
@@ -1,9 +1,17 @@
 package fbp;
 
+import play.Game;
+import play.Play;
+import play.Player;
+
 public class FBPmain {
 
   public static void main(String[] args) {
     System.out.println("I got here");
+    
+    Player player;
+    Play play;
+    Game game;
   }
 
 }
--- FBP/src/play/Play.java
+++ FBP/src/play/Play.java
@@ -1,5 +1,6 @@
 package play;
 
 public class Play {
-
+  private String typeOfPlay;
+  
 }
